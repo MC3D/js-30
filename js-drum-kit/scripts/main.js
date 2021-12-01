9d301e8cd6b7fa0f8e40cb4c100dadcfbaacac93
MC3D <m.chapman.home@gmail.com>
2021-12-01 11:27:53 -0500

    refactors js-drum-kit

--- js-drum-kit/scripts/main.js
+++ js-drum-kit/scripts/main.js
@@ -1,23 +1,17 @@
 function playSound(e) {
-    try {
-        const audio = document.querySelector(`audio[data-key=${e.key.toLowerCase()}]`);
-         if (!audio) return;
-        const key = document.querySelector(
-          `[data-key="${e.key.toLowerCase()}"]`
-        );
-        audio.currentTime = 0;
-        audio.play();
-        key.classList.add("playing");
-    } catch {
-        return;
-    }
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
 }
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
-  e.target.classList.remove("playing");
+  this.classList.remove("playing");
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 window.addEventListener("keydown", playSound);
